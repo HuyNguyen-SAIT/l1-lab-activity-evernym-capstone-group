67a85acd2a7813cbd896fe38b29bb566219e9716
806935 <806935@ELW9IT223070.mshome.net>
2020-02-25 14:38:20 -0700

    Updated form action to send user back to View mode from Edit.

---
+++ User.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.beans.*;
+import java.io.Serializable;
+
+/**
+ *
+ * @author 806935
+ */
+public class User implements Serializable {
+
+    public static final String PROP_SAMPLE_PROPERTY = "sampleProperty";
+
+    private String sampleProperty;
+
+    private PropertyChangeSupport propertySupport;
+
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User() {
+        propertySupport = new PropertyChangeSupport(this);
+    }
+
+    public User(String userName, String firstName, String lastName, String email) {
+        super();
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return userName + " " + firstName + " " + lastName + " " + email;
+    }
+
+    public String getSampleProperty() {
+        return sampleProperty;
+    }
+
+    public void setSampleProperty(String value) {
+        String oldValue = sampleProperty;
+        sampleProperty = value;
+        propertySupport.firePropertyChange(PROP_SAMPLE_PROPERTY, oldValue, sampleProperty);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertySupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertySupport.removePropertyChangeListener(listener);
+    }
+
+}
--- web/WEB-INF/test.jsp
+++ web/WEB-INF/test.jsp
@@ -9,9 +9,45 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Lecture Activity</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Manage Users</h1>
+        <br/>
+        <form method="POST"/>
+        <h3>Users</h3>
+        <table class="tg">
+            <tr>
+                <th class="tg-0pky">Username</th>
+                <th class="tg-0lax">First Name</th>
+                <th class="tg-0lax">Last Name</th>
+                <th class="tg-0lax">Email</th>
+                <th class="tg-0lax">Delete</th>
+                <th class="tg-0lax">Edit</th>
+            </tr>
+            <tr>
+                <td class="tg-0lax"></td>
+                <td class="tg-0lax"></td>
+                <td class="tg-0lax"></td>
+                <td class="tg-0lax"></td>
+                <td class="tg-0lax"></td>
+                <td class="tg-0lax"></td>
+            </tr>
+        </table>
+        
+        <h3>Add User</h3>
+        Username: <input type="text" name="userName"/>
+        <br/>
+        First Name: <input type="text" name="firstName"/>
+        <br/>
+        Last Name: <input type="text" name="lastName"/>
+        <br/>
+        Password: <input type="password" name="password"/>
+        <br/>
+        Email: <input type="text" name="email"/>
+        <br/>
+        <input type="submit" value="Save"/>
+        <form/>
+        <br/>
     </body>
 </html>
